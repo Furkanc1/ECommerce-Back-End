b938deacdec2bf44d0b4ed272de3dc84a311125c
Furkan Coskun <furkdurk7@aol.com>
2023-12-09 14:36:19 -0500

    FINALLY got MySqlWorkbench to create tables and populate them with information!
    (changes made in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,22 @@
 DROP DATABASE IF EXISTS ecommerce_db;
 CREATE DATABASE ecommerce_db;
 
+USE ecommerce_db;
+
 -- the table below stores information about product categories.
 CREATE TABLE category (
   category_id INT NOT NULL AUTO_INCREMENT,
   category_name VARCHAR(255) NOT NULL,
   PRIMARY KEY (category_id)
 );
+
+INSERT INTO category (category_name) VALUES
+  ('Toys'),
+  ('Books'),
+  ('Clothes');
+
+-- selects all rows from the category table
+SELECT * FROM category;
 
 -- stores information about individual products
 CREATE TABLE product (
@@ -19,12 +29,28 @@ CREATE TABLE product (
   FOREIGN KEY (category_id) REFERENCES category(category_id)
 );
 
+INSERT INTO product (product_name, price, stock, category_id) VALUES
+  ('He-Man Figurine', 12.99, 5, 1),
+  ('The Giver', 8.97, 10, 2),
+  ('Sweater', 16.99, 8, 3);
+
+-- selects all rows from the product table
+SELECT * FROM product;
+
 -- stores information about the product tags
 CREATE TABLE tag (
   id INT NOT NULL AUTO_INCREMENT,
   tag_name VARCHAR(255) NOT NULL,
   PRIMARY KEY (id)
 );
+
+INSERT INTO tag (tag_name) VALUES
+  ('ComicBook-Character'),
+  ('Fictional'),
+  ('Comfy');
+
+-- selects all rows from the tag table
+SELECT * FROM tag;
 
 -- represents the many-to-many relationship between products and tags
 CREATE TABLE product_tag (
@@ -36,3 +62,10 @@ CREATE TABLE product_tag (
   FOREIGN KEY (tag_id) REFERENCES tag(id)
 );
 
+INSERT INTO product_tag (product_id, tag_id) VALUES
+  (1, 1),
+  (2, 2),
+  (3, 3);
+
+-- selects all rows from the product_tag table
+SELECT * FROM product_tag;
