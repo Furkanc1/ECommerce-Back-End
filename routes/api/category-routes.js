511444cfedcac2e8f9c38315c9323811d3a6afb9
Furkan Coskun <furkdurk7@aol.com>
2023-12-10 18:44:39 -0500

    created API routes for Product-Routes.js (get put post and delete)

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,6 +1,74 @@
-const express = require('express');
+const express = require(`express`);
 const router = express.Router();
+const { Category, Product } = require(`../../models`);
 
-// need to add routes code here still!
+// gets  all the categories that have associating products
+router.get(`/categories`, async ( req, res ) => {
+  try {
+    const categories = await Category.findAll({ include: [Product] });
+    res.json(categories);
+  } catch ( error ) {
+    console.log(`Error with Category Routes`, error);
+    res.status(500).json({ error: `Error with server` });
+  }
+});
+
+// get a specific category strictly by id of associating products
+router.get(`/categories/:id`, async ( req, res ) => {
+  try {
+    const categoryID = req.params.id;
+    const category = await Category.findOne({
+      where: { id: categoryID },
+      include: [Product],
+    });
+    if (category) {
+      res.json(category);
+    } else {
+      res.status(404).json({ error: `Category does not exist` });
+    }
+  } catch ( error ) {
+        console.log('Error fetching category by ID:', error);
+        res.status(500).json({ error: 'server error' });
+    }
+});
+
+// creates a new category
+router.post(`/categories/:id`, async ( req, res ) => {
+    try {
+        const newCategory = await Category.create(req.body);
+        res.status(201).json(newCategory);
+    } catch ( error ) {
+        console.log(`Error creating new Category:`, error);
+        res.status(400).json({ error: `data is not valid` });
+    }
+});
+
+// will update an existing category by its ID
+router.put(`/categories/:id`, async ( req, res ) => {
+    try {
+        const categoryID = req.params.id;
+        const updatedCategory = await Category.update(req.body, {
+            where: {id: categoryID},
+        });
+        res.status(200).json(updatedCategory);
+    } catch ( error ) {
+        console.log(`error updating category through id:`, error);
+        res.status(400).json({ error: `invalid data or category does not exist` });
+    }
+});
+
+// deletes a category (by id)
+router.delete(`/categories/:id`, async ( req, res ) => {
+    try {
+        const categoryID = req.params.id;
+        const deletedCategory = await Category.destroy({
+            where: {id: categoryID },
+        });
+        res.status(200).json(deletedCategory);
+    } catch ( error ) {
+        console.log(`error deleting category`, error);
+        res.status(400).json({ error: `invalid request or category does not exist` });
+    }
+});
 
 module.exports = router;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,6 +1,74 @@
 const express = require('express');
 const router = express.Router();
+const { Product, Category } = require(`../../models`)
 
-// need to add routes code here still!
+// route for getting all of the proucts in a certain category
+router.get(`/product`, async ( req, res ) => {
+    try {
+        const products = await Product.findAll({ include: [Category] });
+        res.json(products);
+    } catch ( error ) {
+        console.log(`error getching products`, error );
+        res.status(500).json({ error: `server error` });
+    }
+});
+
+// will get a product by ID (with its associating category)
+router.get(`/products/:id`, async ( req, res ) => {
+    try{
+        const productID = req.params.id;
+        const product = await Product.findOne({
+            where: { id: productID }, include: [Category],
+        });
+
+        if (product) {
+            res.json(product);
+        } else {
+            res.status(404).json({ error: `Product wasnt found` });
+        }
+    } catch (error) {
+        console.log(`Error getting product by ID:`, error);
+        res.status(500).json({ error: `server error` });
+    }
+});
+
+// creates new product
+router.post(`/products`, async ( req, res ) => {
+    try{
+        const newProduct = await Product.create(req.body);
+        res.status(201).json(newProduct);
+    } catch (error) {
+        console.log(`error creating new product`, error);
+        res.status(400).json({ error: `invalid data input` });
+    }
+});
+
+// updates a product using its ID
+router.put(`/products/:id`, async ( req, res ) => {
+    try{
+        const productID = req.params.id;
+        const updatedProduct = await Product.update(req.body, {
+            where: { id: productID },
+        });
+        res.status(200).json( updatedProduct );
+    } catch (error) {
+        console.log(`error updating product via id:`, error);
+        res.status(400).json({ error: `invalid data provided or product does not exist` });
+    }
+});
+
+// will delete a product by its ID
+router.delete(`/products/:id`, async ( req, res ) => {
+    try{
+        const productID = req.params.id;
+        const deletedProduct = await Product.destroy({
+            where: { id: productID },
+        });
+        res.status(200).json( deletedProduct );
+    } catch (error) {
+        console.log(`error deleting product by its ID:`, error);
+        res.status(400).json({ error: `invalid request / product not found` });
+    }
+});
 
 module.exports = router;
